5362c5f93664715dbfcba33c0472860d6d95f8d8
1970Mr <mrepoch1970@gmail.com>
2024-02-11 22:13:08 +0330

    Fixed some bugs in TodoLocalStorage

--- resources/js/Services/TodoLocalStorage.js
+++ resources/js/Services/TodoLocalStorage.js
@@ -1,32 +1,34 @@
 class TodoLocalStorage {
   static get() {
-    const todos = localStorage.getItem('todos');
-    return Promise.resolve(JSON.parse(todos));
+    let todos = localStorage.getItem('todos');
+    todos = todos ? JSON.parse(todos) : []
+    return Promise.resolve(todos);
   }
 
-  static store(data) {
-    const todos = this.get();
-    const newTodo = { ...data, id: this._generateUniqueId() };
+  static async store(data) {
+    const todos = await this.get();
+    const newTodo = { id: this._generateUniqueId(), text: data.text, completed: data.completed };
     todos.unshift(newTodo);
     localStorage.setItem('todos', JSON.stringify(todos));
     return Promise.resolve(newTodo);
   }
 
-  static destroy(id) {
-    const todos = this.get();
+  static async destroy(id) {
+    const todos = await this.get();
     const updatedTodos = todos.filter(todo => todo.id !== id);
     localStorage.setItem('todos', JSON.stringify(updatedTodos));
     return Promise.resolve(true);
   }
 
-  static update(data) {
-    const todos = this.get();
-    const updatedTodos = todos.map(todo => (todo.id === data.id ? data : todo));
+  static async update(data) {
+    const todos = await this.get();
+    const updatedTodo = { id: data.id, text: data.text, completed: data.completed };
+    const updatedTodos = todos.map(todo => (todo.id === data.id ? updatedTodo : todo));
     localStorage.setItem('todos', JSON.stringify(updatedTodos));
     return Promise.resolve(data);
   }
 
-  _generateUniqueId() {
+  static _generateUniqueId() {
     return Date.now().toString(36) + Math.random().toString(36).substring(2, 12).padStart(12, 0);
   }
 }
