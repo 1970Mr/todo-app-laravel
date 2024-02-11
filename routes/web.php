96ba84af49dc1c28319d6b3c5738776ff4ae373b
1970Mr <mrepoch1970@gmail.com>
2024-02-11 17:45:52 +0330

    Removing the additional sections of the breeze package

--- routes/web.php
+++ routes/web.php
@@ -8,14 +8,4 @@ use Inertia\Inertia;
 
 Route::get('/', [TodoController::class, 'index'])->name('todo.index');
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
 require __DIR__.'/auth.php';
