46b5738ac409578e7f65da01315cd8adb0396ffd
1970Mr <mrepoch1970@gmail.com>
2025-01-06 07:14:24 +0330

    Update front packages

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,11 @@
 import './bootstrap';
 import '../css/app.css';
 import 'boxicons/css/boxicons.min.css'
-import "vue-awesome-paginate/dist/style.css";
 
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
-import VueAwesomePaginate from "vue-awesome-paginate";
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -18,7 +16,6 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
-            .use(VueAwesomePaginate)
             .mount(el);
     },
     progress: {
